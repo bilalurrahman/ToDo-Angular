717761b1422699bf2e642b7b185e8a61dc059f90
Bilal Ur Rahman <bilal.ur.rahman@hotmail.com>
2023-07-04 08:23:23 +0300

    -Structure created for the project

--- todo-ui/src/app/app.module.ts
+++ todo-ui/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { RegisterUserComponent } from './Authentication/register-user/register-user.component';
 import { LoginUserComponent } from './Authentication/login-user/login-user.component';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { TaskListComponent } from './Task/task-list/task-list.component';
+import { TaskCreateComponent } from './Task/task-create/task-create.component';
+import { TaskUpdateComponent } from './Task/task-update/task-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterUserComponent,
     LoginUserComponent,
-    NavBarComponent
+    NavBarComponent,
+    TaskListComponent,
+    TaskCreateComponent,
+    TaskUpdateComponent
   ],
   imports: [
     BrowserModule,
